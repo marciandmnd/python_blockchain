5adba6336c4fb936aeacfa86c796bc5029739051
Marcian Diamond <marciandmnd@yahoo.com>
2018-05-10 14:28:29 -0700

    Add Block class

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ import json
 import pickle
 
 from hash_util import hash_string_256, hash_block
+from block import Block
 
 MINING_REWARD = 10
 
@@ -28,31 +29,33 @@ def load_data():
             blockchain = json.loads(file_contents[0][:-1])
             updated_blockchain = []
             for block in blockchain:
-                updated_block = {
-                    'previous_hash': block['previous_hash'],
-                    'index': block['index'],
-                    'proof': block['proof'],
-                    'transactions': [OrderedDict(
-                        [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block['transactions']]
-                }
+                converted_tx = [OrderedDict([
+                    ('sender', tx['sender']),
+                    ('recipient', tx['recipient']),
+                    ('amount', tx['amount'])
+                ]) for tx in block['transactions']]
+
+                updated_block = Block(
+                    block['index'],
+                    block['previous_hash'],
+                    converted_tx,
+                    block['proof'],
+                    block['timestamp']
+                )
+
                 updated_blockchain.append(updated_block)
             blockchain = updated_blockchain
 
             open_transactions = json.loads(file_contents[1])
             updated_transactions = []
+
             for tx in open_transactions:
                 updated_transaction = OrderedDict(
                     [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
                 updated_transactions.append(updated_transaction)
             open_transactions = updated_transactions
     except (IOError, IndexError):
-        GENESIS_BLOCK = {
-            'previous_hash': '',
-            'index': 0,
-            'transactions': [],
-            'proof': 100
-        }
-
+        GENESIS_BLOCK = Block(0, '', [], 100)
         blockchain = [GENESIS_BLOCK]
         open_transactions = []
 
@@ -89,12 +92,12 @@ def proof_of_work():
 
 
 def get_balance(participant):
-    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+    tx_sender = [[tx['amount'] for tx in block.transactions if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
     amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
 
-    tx_recipient= [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+    tx_recipient= [[tx['amount'] for tx in block.transactions if tx['recipient'] == participant] for block in blockchain]
     amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
 
     return amount_received - amount_sent
@@ -145,12 +148,6 @@ def mine_block():
 
     proof = proof_of_work()
 
-    # reward_transaction = {
-    #     'sender': 'MINING',
-    #     'recipient': owner,
-    #     'amount': MINING_REWARD
-    # }
-
     reward_transaction = OrderedDict([
         ('sender', 'MINING'),
         ('recipient', owner),
@@ -160,12 +157,7 @@ def mine_block():
     copied_transactions = open_transactions[:]
     copied_transactions.append(reward_transaction)
 
-    block = {
-        'previous_hash': hashed_block,
-        'index': len(blockchain),
-        'transactions': copied_transactions,
-        'proof': proof
-    }
+    block = Block(len(blockchain), hashed_block, copied_transactions, proof)
     blockchain.append(block)
     return True
 
@@ -191,9 +183,9 @@ def verify_chain():
     for (index, block) in enumerate(blockchain):
         if index == 0:
             continue
-        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+        if block.previous_hash != hash_block(blockchain[index - 1]):
             return False
-        if not valid_proof(block['transactions'][:-1], block['previous_hash'], block['proof']):
+        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
             print('Proof of work invalid')
             return False
     return True
@@ -210,7 +202,6 @@ while waiting_for_input:
     print('3: Output the blockchain blocks')
     print('4: Output participants')
     print('5: Check transaction validity')
-    print('h: Manipulate the chain')
     print('q: quit')
 
     user_choice = get_user_choice()
@@ -235,13 +226,6 @@ while waiting_for_input:
             print('All transactions valid')
         else:
             print('Invalid transactions present')
-    elif user_choice == 'h':
-        if len(blockchain) >= 1:
-            blockchain[0] = {
-                'previous_hash': '',
-                'index': 0,
-                'transactions': [{'sender': 'Chris', 'recipient': 'Max', 'amount': 100}]
-            }
     elif user_choice == 'q':
         waiting_for_input = False
     else:

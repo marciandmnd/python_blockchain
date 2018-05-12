a5c977099542799e7dfb4a23791554e1557af6fd
Marcian Diamond <marciandmnd@yahoo.com>
2018-05-11 21:03:34 -0700

    use private attributes in blockchain

--- node.py
+++ node.py
@@ -19,7 +19,7 @@ class Node:
         return user_input
 
     def print_blockchain_elements(self):
-        for block in self.blockchain.chain:
+        for block in self.blockchain.get_chain():
             print('Outputting block...')
             print(block)
         else:
@@ -44,13 +44,13 @@ class Node:
                     print('Added transaction')
                 else:
                     print('Transaction failed')
-                print(self.blockchain.open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif user_choice == '2':
                 self.blockchain.mine_block()
             elif user_choice == '3':
                 self.print_blockchain_elements()
             elif user_choice == '4':
-                if Verification.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print('All transactions valid')
                 else:
                     print('Invalid transactions present')
@@ -58,7 +58,7 @@ class Node:
                 waiting_for_input = False
             else:
                 print('Input was invalid, please pick a value from the list!')
-            if not Verification.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.get_chain()):
                 print('Invalid blockchain!')
                 break
             print('Balance of {}: {:6.2f}'.format(

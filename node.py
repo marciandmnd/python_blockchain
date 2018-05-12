9e63941d736bdb97bc2b9e5bbb3154778d3db305
Marcian Diamond <marciandmnd@yahoo.com>
2018-05-11 20:56:59 -0700

    Use static and class methods in Verification class

--- node.py
+++ node.py
@@ -50,8 +50,7 @@ class Node:
             elif user_choice == '3':
                 self.print_blockchain_elements()
             elif user_choice == '4':
-                verifier = Verification()
-                if verifier.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
+                if Verification.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
                     print('All transactions valid')
                 else:
                     print('Invalid transactions present')
@@ -59,8 +58,7 @@ class Node:
                 waiting_for_input = False
             else:
                 print('Input was invalid, please pick a value from the list!')
-            verifier = Verification()
-            if not verifier.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.chain):
                 print('Invalid blockchain!')
                 break
             print('Balance of {}: {:6.2f}'.format(
